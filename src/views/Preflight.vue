0cb546bff29b5716651794807f98fc65de11ead7
Ridho Maulana <edoin21@gmail.com>
2019-09-26 22:15:59 +0700

    Fix linting

--- src/views/Preflight.vue
+++ src/views/Preflight.vue
@@ -13,18 +13,27 @@
     <main class="max-w-lg mx-auto w-auto flex-grow">
       <div class="rounded-lg bg-white shadow-lg mx-3 -mt-48 mb-10">
         <div class="px-3 pt-3 pb-1">
-          <img src="../assets/cute.webp" alt="cute cats" class="w-full"/>
-          <div v-if="urlError" class="text-lg py-8 text-gray-700 font-medium text-center">
+          <img src="../assets/cute.webp" alt="cute cats" class="w-full" />
+          <div
+            v-if="urlError"
+            class="text-lg py-8 text-gray-700 font-medium text-center"
+          >
             {{ urlError }}
           </div>
           <span v-else class="text-center pb-10">
             <p class="text-lg pt-10 pb-2 text-gray-700 font-medium">
               Redirecting you to
             </p>
-            <span v-if="longURL" class="bg-gray-300 py-1 px-2 mx-4 mb-8 rounded truncate block">
+            <span
+              v-if="longURL"
+              class="bg-gray-300 py-1 px-2 mx-4 mb-8 rounded truncate block"
+            >
               {{ longURL }}
             </span>
-            <span v-else class="bg-gray-300 py-1 px-2 mx-4 mb-8 rounded truncate block">
+            <span
+              v-else
+              class="bg-gray-300 py-1 px-2 mx-4 mb-8 rounded truncate block"
+            >
               <span v-for="n in 6" :key="n">&middot; </span>
             </span>
           </span>
